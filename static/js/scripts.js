3e64e8f8238a6b46a8631df7fabe98acdcdac189
Luca Leonardo Bologna <lucaleonardo.bologna@pa.ibf.cnr.it>
2021-04-26 19:08:45 +0200

    minor refactoring

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -25,10 +25,10 @@
 	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
 		if ($(".navbar").offset().top > 100 && $("#navbar-toggler-btn").is(':hidden')) {
-			$("#eb-logo").attr("src", "../images/logos/ebrains_logos_yellow_green-black.png");
+			$("#eb-logo").attr("src", "./static/images/logos/ebrains_logos_yellow_green_black.png");
 			$(".fixed-top").addClass("top-nav-collapse");
 		} else {
-			$("#eb-logo").attr("src", "../images/logos/ebrains_logos_yellow_green-white.png");
+			$("#eb-logo").attr("src", "./static/images/logos/ebrains_logos_yellow_green_white.png");
 			$(".fixed-top").removeClass("top-nav-collapse");
 		}
     });
